0b5b131eda9f3d57de334a338b6ea0e370e911d3
Anton Lukin <anton@lukin.me>
2022-05-06 17:34:25 +0400

    fix delay strategy for live

--- helpers/columns.js
+++ helpers/columns.js
@@ -1,5 +1,19 @@
 module.exports = (history) => {
+  const places = {};
+
+  history.forEach(item => {
+    if (places[item.coords]) {
+      places[item.coords].delay += item.delay;
+
+      return;
+    }
+
+    places[item.coords] = item;
+  });
+
   const data = [];
+
+  history = Object.values(places);
 
   // Find history half
   const half = Math.ceil(history.length / 2);
--- helpers/compose.js
+++ helpers/compose.js
@@ -1,20 +1,18 @@
-const differ = require('date-differ');
+const differ = require('datetime-diff');
 
 module.exports = (field) => {
   if (field.to === null) {
     field.to = new Date();
   }
 
+  const between = differ(field.from, field.to);
+
   const compose = {
     place: field.place,
     coords: field.coords,
-    delay: differ({from: field.from, to: field.to, days: true}),
+    delay: Math.floor(between.days),
     link: `https://www.google.com/maps/@${field.coords},12z`,
   };
-
-  if (compose.delay === 'Dates are the same day') {
-    compose.delay = '1 day';
-  }
 
   return compose;
 };
